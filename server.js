81c5b7da52c9d104564fd5e13e485f5be3320ca0
ducky <chrisdmle@gmail.com>
2022-08-19 11:36:36 -0700

    updating mongodb connections

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(routes);
 
-app.use(require('./routes'));
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
-
-mongoose.set('debug', true);
-
-
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
